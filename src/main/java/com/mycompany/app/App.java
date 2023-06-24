ff62c9f89a42aef0759085dfbe7021a455fd5eff
joshna <jyothsnakasireddy04@gmail.com>
2023-06-24 17:46:51 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! this is test trigger";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
